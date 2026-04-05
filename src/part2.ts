ab420f144e47fe11a477565de9b2e87f8a0e0812
lior4715 <lior4715@gmail.com>
2026-04-05 12:52:17 +0300

    Implement countVowels, isPalindrome, and treeToSentence functions

--- src/part2.ts
+++ src/part2.ts
@@ -4,10 +4,25 @@ const stringToArray = R.split("");
 
 /* Question 2.1 */
 const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
-export const countVowels: (s: string) => number = undefined as any;;
+export const countVowels: (s: string) => number = R.pipe(
+    stringToArray,
+    R.map(R.toLower),
+    R.filter((c: string) => R.includes(c, vowels)),
+    R.length
+);
 
 /* Question 2.2 */
-export const isPalindrome = (text: string): boolean => undefined as any;;
+export const isPalindrome = (text: string): boolean => {
+    
+    const cleaned = R.pipe(
+        stringToArray,
+        R.map(R.toLower),
+        R.filter(R.test(/[a-z0-9]/))
+    )(text);
+
+    const half = Math.floor(cleaned.length / 2);
+    return R.all((i: number) => cleaned[i] === cleaned[cleaned.length - 1 - i], R.range(0, half));
+};
   
 
 /* Question 2.3 */
@@ -16,4 +31,9 @@ export type WordTree = {
     children: WordTree[];
 }
 
-export const treeToSentence = (t: WordTree): string => undefined as any;;
+export const treeToSentence = (t: WordTree): string => {
+    if (t.children.length === 0) {
+        return t.root;
+    }
+    return t.root + " " + t.children.map(treeToSentence).join(" ");
+};
